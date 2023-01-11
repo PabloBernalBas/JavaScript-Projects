f11a78b91f011e1c4daa02c241f08b835fc375c2
PabloBernalBas <polberny79@outlook.com>
2023-01-11 11:27:02 +0000

    assignment

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,7 +1,7 @@
 var Family = "Sympsons", Dad = "Hommer", Mom = "Marge";
 document.write(Mom)
-
+break
 var blues = "I have the blues";
 document.write('<font color="red"> blue</font>');
-
+break
 document.write(3+3)
