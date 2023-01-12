a14ae36ba67abf6403c79d9ca0f779a8fb22df5b
PabloBernalBas <polberny79@outlook.com>
2023-01-12 13:13:37 +0000

    assignment

--- Project3_math_operators/main3.js
+++ Project3_math_operators/main3.js
@@ -2,3 +2,18 @@ function addition_Function() {
     var addition = 2 + 2;
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
+
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math").innerHTML = "6 * 8 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+}
