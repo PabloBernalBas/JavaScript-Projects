d4dbb4aa5aefdc1e2d6d02c46ae3399f8b3213b0
PabloBernalBas <polberny79@outlook.com>
2023-01-12 13:27:34 +0000

    assignment

--- Project3_math_operators/main3.js
+++ Project3_math_operators/main3.js
@@ -17,3 +17,14 @@ function division() {
     var simple_Math = 48 / 6;
     document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
 }
+
+function more_Math() {
+    var simple_Math = 1 + 2;
+    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
