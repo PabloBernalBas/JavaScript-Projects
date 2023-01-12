e8ab91b06ffbf362ae32ee1768f1c683876e4b99
PabloBernalBas <polberny79@outlook.com>
2023-01-12 16:59:32 +0000

    assignemtn

--- Project3_math_operators/main3.js
+++ Project3_math_operators/main3.js
@@ -1,33 +1,37 @@
 function addition_Function() {
     var addition = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+    document.getElementById("Math").innerHTML = " " + addition;
 }
 
 function subtraction_Function() {
     var subtraction = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
+    document.getElementById("Math").innerHTML = " " + subtraction;
 }
 
 function multiplication() {
     var simple_Math = 6 * 8;
-    document.getElementById("Math").innerHTML = "6 * 8 = " + simple_Math;
+    document.getElementById("Math").innerHTML = " " + simple_Math;
 }
 
 function division() {
     var simple_Math = 48 / 6;
-    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+    document.getElementById("Math").innerHTML = " " + simple_Math;
 }
 
 function more_Math() {
-    var simple_Math = (1 + 2) * / 2 - 5;
-    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, diided in half  and then subtracted by 5 equals= " + simple_Math;
+    var simple_Math = (1 + 2) * 10 / 2 - 5;
+    document.getElementById("Math").innerHTML = " " + simple_Math;
 }
 
 function modulus_Operator() {
     var simple_Math = 25 % 6;
-    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+    document.getElementById("Math").innerHTML = " " + simple_Math;
 }
 
 var X = 5;
 X++;
-document.write(X);
+return(X);
+
+var Y = 5;
+Y--;
+document.write(Y);
