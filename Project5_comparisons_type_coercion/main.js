2adaea348579e243b0e7203798488c221e7437a1
PabloBernalBas <polberny79@outlook.com>
2023-01-14 17:17:02 +0000

    Assignment

--- Project5_comparisons_type_coercion/main.js
+++ Project5_comparisons_type_coercion/main.js
@@ -4,6 +4,10 @@ document.write("<br>");
 
 document.write(2E310);
 
+document.write("<br>");
+
+document.write(-3E310)
+
 function my_Function() {
     document.getElementById("Test").innerHTML = 0/0;
 }
