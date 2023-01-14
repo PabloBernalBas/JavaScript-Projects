18423658f5165b4ffc20abe558fb363412818951
PabloBernalBas <polberny79@outlook.com>
2023-01-14 16:46:15 +0000

    Assignment

--- Project4_dictionaries/index.html/main.js
+++ Project4_dictionaries/index.html/main.js
@@ -1,11 +1,11 @@
-function my_Dictionary() {
-    var Animal = {
-        Species:"Dog",
-        Color:"Black",
-        Breed:"Labrador",
-        Age:5,
-        Sound:"Bark!"
+function my_Dictionary() { //defining the function and giving a name
+    var Animal = {  //defining the variable and giving a name an object
+        Species:"Dog", //defining KVP
+        Color:"Black", //""
+        Breed:"Labrador", //""
+        Age:5, //""
+        Sound:"Bark!" //""
     };
-    delete Animal.Breed;
-    document.getElementById("Dictionary").innerHTML=Animal.Breed;
+    delete Animal.Breed; //deleting a statement request
+    document.getElementById("Dictionary").innerHTML=Animal.Breed; //method to returns and element object, which property matches with the string.
 }
