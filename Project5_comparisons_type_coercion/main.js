4576e7570dd02c8732cf254de3140d48ff0e5d59
PabloBernalBas <polberny79@outlook.com>
2023-01-14 17:12:00 +0000

    Assignment

---
+++ Project5_comparisons_type_coercion/main.js
@@ -0,0 +1,14 @@
+document.write(typeof "Word");
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+function my_Function1() {
+    document.getElementById("Test").innerHTML = isNaN('Is this True');
+}
+
+function my_Function2() {
+    document.getElementById("Test").innerHTML = isNaN('34');
+}
+
