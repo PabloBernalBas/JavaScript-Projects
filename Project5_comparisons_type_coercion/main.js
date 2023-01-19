85642a5dde08934c28b6c8913b27e1981172406a
PabloBernalBas <polberny79@outlook.com>
2023-01-19 03:20:29 +0000

    Assignment

--- Project5_comparisons_type_coercion/main.js
+++ Project5_comparisons_type_coercion/main.js
@@ -8,13 +8,19 @@ document.write("<br>");
 
 document.write(-3E310)
 
+document.write("<br>");
+
 function my_Function() {
     document.getElementById("Test").innerHTML = 0/0;
 }
 
+document.write("<br>");
+
 function my_Function1() {
     document.getElementById("Test1").innerHTML = isNaN('Is this True');
 }
+
+document.write("<br>");
 
 function my_Function2() {
     document.getElementById("Test2").innerHTML = isNaN('34');
@@ -69,3 +75,29 @@ y=2
 document.write(x===y);
 
 document.write("<br>");
+
+document.write((5 < 10 && 5 > 1));
+
+document.write("<br>");
+
+document.write((5 > 10 && 5 > 1));
+
+document.write("<br>");
+
+document.write((5 > 10 || 5 > 1));
+
+document.write("<br>");
+
+document.write((5 > 10 || 5 < 1));
+
+document.write("<br>");
+
+function not_Function(){
+    document.getElementById("Not").innerHTML=!(5>10);
+}
+
+document.write("<br>");
+
+function not_Function1(){
+    document.getElementById("Not1").innerHTML=!(20>10);
+}
