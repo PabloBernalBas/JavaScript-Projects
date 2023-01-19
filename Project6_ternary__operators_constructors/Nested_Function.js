27a14f3afd1981ea1e2953559b425e218c14477f
PabloBernalBas <polberny79@outlook.com>
2023-01-19 15:37:03 +0000

    Assignment

---
+++ Project6_ternary__operators_constructors/Nested_Function.js
@@ -0,0 +1,13 @@
+var up = document.getElementById('UP');
+        var down = document.getElementById('DOWN');
+        up.innerHTML =
+            "Click on the button to call nested function.";    
+        function fun1(a) {
+        fun = function fun2(b) {
+            return a + b;
+        }
+        return fun;
+        }
+        function Nested_Function() {
+            down.innerHTML = fun1("This is ")("the result.");
+        }
