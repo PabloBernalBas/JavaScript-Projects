9afa8db060b99051f67c1a2b51aa053b927252eb
PabloBernalBas <polberny79@outlook.com>
2023-01-19 03:46:16 +0000

    Assignment

--- Project6_ternary__operators_constructors/main.js
+++ Project6_ternary__operators_constructors/main.js
@@ -1,7 +1,7 @@
 function Age_Function() {
     var Vote, Can_vote;
     Vote = document.getElementById("Vote").value;
-    Can_vote = (Vote < 21) ? "You are not old enough":"You are old enough"
+    Can_vote = (Vote < 18) ? "You are not old enough":"You are old enough"
     document.getElementById("Age").innerHTML=Can_vote + ".";
 }
 
