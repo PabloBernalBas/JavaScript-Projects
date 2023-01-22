3b11cda2151b0aa77a0d28c5a666d6743498ce57
PabloBernalBas <polberny79@outlook.com>
2023-01-22 16:16:42 +0000

    a

---
+++ Project8_string_methods/Project8_string_methods.js
@@ -0,0 +1,9 @@
+function full_Sentence() {
+    var part_1 = " "
+    var part_2 = " "
+    var part_3 = " "
+    var part_4 = " "
+    var part_5 = " "
+    var whole_sentence  = part_1(part_2, part_3, part_4, part_5);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
