9f9596a728e0ec0a73729916a1593d79d7efc745
PabloBernalBas <polberny79@outlook.com>
2023-01-22 04:36:09 +0000

    Assignment

--- Project6_ternary__operators_constructors/Project6_ternary_operators_constructors.js
+++ Project6_ternary__operators_constructors/Project6_ternary_operators_constructors.js
@@ -1,36 +1,37 @@
-var up = document.getElementById('UP');
-var down = document.getElementById('DOWN'); 
+var up = document.getElementById('UP');//defining the variable and giving a name inside a method
+var down = document.getElementById('DOWN'); //defining the variable and giving a name inside a method
     up.innerHTML = "Click on the button, to call nested function.";    
-        function fun1(a) {
-        fun = function fun2(b) {
-            return a + b;
+        function fun1(a) { //defining a function and give it a name and value
+        function fun2(b) { //defining a function and give it a name and value
+            return a + b; //requested a return value
         }
-        return fun;
+        return fun2; //requested a return value
         }
-    function Nested_Function() {
-            down.innerHTML = fun1("This is ")("the result.");
+    function Nested_Function() { //defining a function and give it a name
+        down.innerHTML = fun1("This is ")("the result."); //method to returns and element object, which property matches with the string.
         }
 
-document.write("<br>")
+document.write("<br>") //making space in between functions to tide it up
 
-function Worker (Name, Age, Sex, Position) {
-    this.Worker_Name = Name;
-    this.Worker_Age = Age;
-    this.Worker_Sex = Sex;
-    this.Worker_Position = Position
+function Worker (Name, Age, Sex, Position) {//defining a function and give it a name an Object Array value
+    this.Worker_Name = Name; //giving name to keywords inside of the function
+    this.Worker_Age = Age; //""
+    this.Worker_Sex = Sex; //""
+    this.Worker_Position = Position //""
     }
-    var worker1 = new Worker ("Juan", 23, "Male", "HR");
-    var worker2 = new Worker ("Sara", 32, "Female", "Admin");
-    var worker3 = new Worker ("Luis", 27, "Male", "Finance");
-function myFunction() {
+    var worker1 = new Worker ("Juan", 23, "Male", "HR"); //defining keywords and use it as object
+    var worker2 = new Worker ("Sara", 32, "Female", "Admin");//""
+    var worker3 = new Worker ("Luis", 27, "Male", "Finance");//""
+function myFunction() { //defining a function and give it a name
     document.getElementById("New_and_This").innerHTML = "This is " + worker1.Worker_Name + ", " + worker1.Worker_Age + " years old," + " this employee works in " + worker1.Worker_Position + ".";
+    //method to returns and element object, which property matches with the string.
 }    
 
-document.write("<br>")
+document.write("<br>") //making space in between functions to tide it up
 
-function Vote_Function() {
-    var Age, Can_vote;
-    Age = document.getElementById("Age").value;
-    Can_vote = (Age > 17) ? "You can Vote":"You can't Vote"; 
-    document.getElementById("Vote").innerHTML = Can_vote + ".";
+function Vote_Function() { //defining a function and give it a name
+    var Age, Can_vote; //defining the variables
+    Age = document.getElementById("Age").value; //giving a variable a value
+    Can_vote = (Age > 17) ? "You can Vote":"You can't Vote"; //defining the variable with a condition
+    document.getElementById("Vote").innerHTML = Can_vote + "."; //method to returns and element object, which property matches with the string.
 }
