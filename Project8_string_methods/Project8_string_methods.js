d2d0723d8c9cde941b02b2835f974f519024d48a
PabloBernalBas <polberny79@outlook.com>
2023-01-26 09:00:37 +0000

    a

--- Project8_string_methods/Project8_string_methods.js
+++ Project8_string_methods/Project8_string_methods.js
@@ -1,9 +1,9 @@
 function full_Sentence() {
-    var part_1 = " "
-    var part_2 = " "
-    var part_3 = " "
-    var part_4 = " "
-    var part_5 = " "
-    var whole_sentence  = part_1(part_2, part_3, part_4, part_5);
+    var part_1 = "I have "
+    var part_2 = "made this "
+    var part_3 = "function "
+    var part_4 = "to get all "
+    var part_5 = "sentence together."
+    var whole_sentence  = part_1.concat(part_2, part_3, part_4, part_5);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
