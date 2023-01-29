178620c4fbc8660679a0243f350b511520fe5a66
PabloBernalBas <polberny79@outlook.com>
2023-01-29 10:15:40 +0000

    Assignment

--- Project10_loops_arrays/Project10_loops_arrays.js
+++ Project10_loops_arrays/Project10_loops_arrays.js
@@ -16,3 +16,13 @@ var len = txt.length;
 document.write(len)
 
 document.write("<br>")
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length;  Y++) {
+        Content += Instruments[Y]+ "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
