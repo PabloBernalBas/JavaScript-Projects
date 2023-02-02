dd2e9fa35fb1dcfe44cbea2b0de90a83a71d4a0f
PabloBernalBas <polberny79@outlook.com>
2023-02-02 12:58:34 +0000

    Assignment

--- Project10_loops_arrays/Project10_loops_arrays.js
+++ Project10_loops_arrays/Project10_loops_arrays.js
@@ -1,72 +1,111 @@
-function Call_Loop() {
-    var Digit = "";
-    var x = 10;
+function Call_Loop() { //defining a function and give it a name
+    var Digit = "";  //giving variable a name and value
+    var x = 10;  //giving variable a name and value
     while (x > -1) {
         Digit += "<br>" + x;
         x--
     }
-    document.getElementById("Call_Loop").innerHTML= Digit;
+    document.getElementById("Call_Loop").innerHTML= Digit;  //return value in browser
 }
 
-document.write("<br>")
-document.write("<br>")
+document.write("<br>") //break for cleaner view
+document.write("<br>") //break for cleaner view
 
-var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var len = txt.length;
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  //giving variable a name and value
+var len = txt.length;  //giving variable a name and value
 document.write(len)
 
-document.write("<br>")
+document.write("<br>") //break for cleaner view
 
-var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
-var Content = "";
-var Y;
-function for_Loop() {
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];  //giving variable a name and array as value
+var Content = "";  //giving variable a name and value
+var Y; //giving variable a name
+function for_Loop() { //defining a function and give it a name
     for (Y = 0; Y < Instruments.length;  Y++) {
         Content += Instruments[Y]+ "<br>";
     }
-    document.getElementById("List_of_Instruments").innerHTML = Content;
+    document.getElementById("List_of_Instruments").innerHTML = Content;  //return value in browser
 }
 
-document.write("<br>")
+document.write("<br>") //break for cleaner view
 
-function Array_Function () {
-    var Season_Pictures  = [];
-    Season_Pictures[0] = "spring";
-    Season_Pictures[1] = "summer";
-    Season_Pictures[2] = "autumn";
-    Season_Pictures[3] = "winter";
-    document.getElementById("Array").innerHTML = "In this picture, the season is " + Season_Pictures[1] + ".";
+function Array_Function () { //defining a function and give it a name
+    var Season_Pictures  = []; //giving variable a name and value
+    Season_Pictures[0] = "spring"; //giving object name and value
+    Season_Pictures[1] = "summer"; //""
+    Season_Pictures[2] = "autumn"; //""
+    Season_Pictures[3] = "winter"; //""
+    document.getElementById("Array").innerHTML = "In this picture, the season is " + Season_Pictures[1] + ".";  //return value in browser
 }
 
-document.write("<br>")
+document.write("<br>") //break for cleaner view //break for cleaner view
 
-function constant_function() {
+function constant_function() { //defining a function and give it a name
     const Musical_Instrument = {type: "violin", brand:"Stradivarius", color:"blue"};
     Musical_Instrument.color = "Oak";
     Musical_Instrument.price = "£15000";
-    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type +  " was " + Musical_Instrument.price;
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type +  " was " + Musical_Instrument.price;  //return value in browser
 }
 
-document.write("<br>")
+document.write("<br>") //break for cleaner view
 
-var my_name = "Mr Anderson";
+var my_name = "Mr Anderson";  //giving variable a name and value
 document.write(my_name)
 	{
   let my_name = ", AKA 'Neo'.";
 	document.write(my_name)
   }
 
-  document.write("<br>")
+document.write("<br>") //break for cleaner view
 
-  function return_function(a, b){
+function return_function(a, b){ //defining a function and give it a name and variable
     var r;
-    a = 5;
-    b = 3;
+    a = 5; //giving value to variable
+    b = 3; //giving value to variable
     r = a * b;
-    return(r);
-  }
-  
-  let result = return_function();
-  console.log(result);
+return(r);
+}
+let result = return_function();
+console.log(result);  //return value in console
 
-  document.write("<br>")
+document.write("<br>") //break for cleaner view
+
+let car = { //naming an object
+    make: "Dodge", //giving properties name and value
+    model: "Viper", //""
+    year: "2021", //""
+    color: "red", //""
+description : function() {
+    return "The car is a " + this.year + " " + this.color + " " + this.make + " " + this.model + ".";
+}
+};
+document.getElementById("object").innerHTML= car.description();  //return value in browser
+
+document.write("<br>") //break for cleaner view
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"]; //
+let text = "";
+
+list: {
+    text += cars[0] + "<br>"; 
+    text += cars[1] + "<br>"; 
+    break list;
+    text += cars[2] + "<br>"; 
+    text += cars[3] + "<br>"; 
+    }
+
+document.getElementById("demo").innerHTML = text;  //return value in browser
+
+document.write("<br>") //break for cleaner view
+
+let data = '';
+
+for (let i = 0; i < 10; i++) {
+  if (i === 3) {
+    continue;
+  }
+  data = data + i;
+}
+console.log(data);  //return value in console
+
+document.write("<br>") //break for cleaner view
