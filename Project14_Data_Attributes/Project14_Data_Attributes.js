369ff1e547c7b56eebbac472adc1db37668084f5
PabloBernalBas <polberny79@outlook.com>
2023-02-04 15:34:17 +0000

    Assignment

--- Project14_Data_Attributes/Project14_Data_Attributes.js
+++ Project14_Data_Attributes/Project14_Data_Attributes.js
@@ -1,4 +1,4 @@
 function displayType (character) {
     var characterType = character.getAttribute("data-character-type");
-    alert(characterType + " is in the " + character.innerHTML + " universe!");
+    alert(characterType + " are the drivers in the " + character.innerHTML + " Team for 2023 Formula1 season.");
   }
